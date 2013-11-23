d5fcb0e61912d473a86131d82c221e8163ea0e7e
Marc Kettler <marckettler@g.harvard.edu>
2013-11-23 01:15:17 -0500

    score-card.js V0.0.2.1 ------------------------ Changed to local jquery. Added the Innings and Player Boxes. Added a few buttons to test drawing to the Event Boxes.

--- js/score-card.js
+++ js/score-card.js
@@ -9,24 +9,39 @@ function ScoreCard(canvas,batters)
     this.canvas.width = 1000;
     this.canvas.height = 500;
     this.currentAB = 1;
+    this.outs = 0;
+    this.playerBoxes = new Array(batters);
     this.eventBoxes = new Array(batters);
+    this.abNum = 1;
+    this.x = 100;
+    this.y = 0;
+
     for (var i = 0; i < this.eventBoxes.length; i++)
     {
         this.eventBoxes[i] = new Array(10);
     }
-    var abNum = 1;
-    var x = 0;
-    var y = 0;
+
+    //draw innings
+    drawInnings(this.x,this.y);
+    this.y = 25;
+    //draw player boxes
+    for (var i = 0; i < this.eventBoxes.length; i++)
+    {
+        this.playerBoxes[i] = new PlayerBox(canvas,"Player "+(i+1),0,this.y);
+        this.playerBoxes[i].draw();
+        this.y += 50;
+    }
+    this.y=25;
     for (var i = 0; i < this.eventBoxes[0].length; i++)
     {
         for (var j = 0; j < this.eventBoxes.length; j++)
         {
-            this.eventBoxes[j][i] = new EventBox(canvas,abNum++,x,y);
+            this.eventBoxes[j][i] = new EventBox(canvas,this.abNum++,this.x,this.y);
             this.eventBoxes[j][i].draw();
-            y += 50;
+            this.y += 50;
         }
-        x += 50;
-        y = 0;
+        this.x += 50;
+        this.y = 25;
     }
 
     function findEventBox(eventBoxes,boxID)
@@ -44,14 +59,86 @@ function ScoreCard(canvas,batters)
 
     }
 
+    function drawInnings(x,y)
+    {
+        var width = 50;
+        var height = 25;
+        var ctx = canvas.getContext("2d");
+        ctx.font = height/2+'px Sans-Serif';
+        for(var i=0;i<10;i++)
+        {
+            ctx.fillText  ((i+1).toString(), x+(width/3), y+(height/1.33));
+            ctx.strokeRect(x,y,width,height);
+            x += 50;
+        }
+    }
 
-    this.processAB = processAB;
-    function processAB()
+    this.single = single;
+    function single()
     {
         var currentEventBox = findEventBox(this.eventBoxes,this.currentAB);
         currentEventBox.onFirst();
         this.currentAB++;
     }
+
+    this.double = double;
+    function double()
+    {
+        var currentEventBox = findEventBox(this.eventBoxes,this.currentAB);
+        currentEventBox.onFirst();
+        currentEventBox.onSecond();
+        this.currentAB++;
+    }
+
+    this.triple = triple;
+    function triple()
+    {
+        var currentEventBox = findEventBox(this.eventBoxes,this.currentAB);
+        currentEventBox.onFirst();
+        currentEventBox.onSecond();
+        currentEventBox.onThird();
+        this.currentAB++;
+    }
+
+    this.homerun = homerun;
+    function homerun()
+    {
+        var currentEventBox = findEventBox(this.eventBoxes,this.currentAB);
+        currentEventBox.onFirst();
+        currentEventBox.onSecond();
+        currentEventBox.onThird();
+        currentEventBox.runScored();
+        this.currentAB++;
+    }
+
+    this.walk = walk;
+    function walk()
+    {
+        var currentEventBox = findEventBox(this.eventBoxes,this.currentAB);
+        currentEventBox.onFirst();
+        this.currentAB++;
+    }
+}
+
+function PlayerBox(canvas,name,x,y)
+{
+    var width = 100;
+    var height = 25;
+    this.x = x;
+    this.y = y;
+    this.canvas = canvas;
+    this.name = name;
+
+    this.draw = draw;
+    function draw()
+    {
+        var ctx = canvas.getContext("2d");
+        ctx.font = height/2+'px Sans-Serif';
+        ctx.fillText  (this.name,x+(width/3), y+(height/1.33));
+        ctx.strokeRect(x,y,width,height);
+        ctx.strokeRect(x,y+25,width,height);
+    }
+
 }
 
 function EventBox(canvas,abNum,x,y)
@@ -84,7 +171,7 @@ function EventBox(canvas,abNum,x,y)
         var originalFill = this.ctx.fillStyle;
         // draw mound - will be black if a run scored.
         this.ctx.beginPath();
-        this.ctx.arc(this.x+(BOX_W_H/2), this.y+(BOX_W_H/2), BOX_W_H/10, 0, 2 * Math.PI, false);
+        this.ctx.arc(this.x+(BOX_W_H/2), this.y+(BOX_W_H/2), BOX_W_H/12, 0, 2 * Math.PI, false);
         this.ctx.fillStyle = color;
         this.ctx.fill();
         this.ctx.fillStyle = originalFill;
@@ -108,10 +195,10 @@ function EventBox(canvas,abNum,x,y)
     function onSecond()
     {
         //make line slightly bigger
-        this.ctx.lineWidth = 2;
+        this.ctx.lineWidth = 3;
         //Path for line from home to 2nd.
         this.ctx.beginPath();
-        this.ctx.moveTo(this.x(BOX_W_H*(3/4)),this.y+(BOX_W_H/2));
+        this.ctx.moveTo(this.x+(BOX_W_H*(3/4)),this.y+(BOX_W_H/2));
         this.ctx.lineTo(this.x+(BOX_W_H/2),this.y+(BOX_W_H/4));
         this.ctx.stroke();
         //reset original lineWidth
@@ -122,11 +209,11 @@ function EventBox(canvas,abNum,x,y)
     function onThird()
     {
         //make line slightly bigger
-        this.ctx.lineWidth = 2;
+        this.ctx.lineWidth = 3;
         //Path for line from home to 2nd.
         this.ctx.beginPath();
-        this.ctx.moveTo(this.x(BOX_W_H/2),this.y(BOX_W_H/4));
-        this.ctx.lineTo(this.x+(BOX_W_H/4),this.y(BOX_W_H/2));
+        this.ctx.moveTo(this.x+(BOX_W_H/2),this.y+(BOX_W_H/4));
+        this.ctx.lineTo(this.x+(BOX_W_H/4),this.y+(BOX_W_H/2));
         this.ctx.stroke();
         //reset original lineWidth
         this.ctx.lineWidth = 1;
@@ -146,13 +233,13 @@ function EventBox(canvas,abNum,x,y)
     function runScored()
     {
         //make line slightly bigger
-        this.ctx.lineWidth = 2;
+        this.ctx.lineWidth = 3;
         //Path for line from 3rd to home.
         this.ctx.beginPath();
         this.ctx.moveTo(this.x+(BOX_W_H/4),this.y+(BOX_W_H/2));
         this.ctx.lineTo(this.x+(BOX_W_H/2),this.y+(BOX_W_H*(3/4)));
         this.ctx.stroke();
-        drawMound("black");
+        this.drawMound("black");
         //reset original lineWidth
         this.ctx.lineWidth = 1;
     }
